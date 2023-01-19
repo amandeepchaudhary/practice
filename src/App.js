29d63180b63bd06d1b734bedacda389a7da6e05d
amandeep <amanchaudhary.ac1@gmail.com>
2023-01-19 23:15:44 +0530

    onclick event with setState by Class Method

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      firstName: "",
+      lastName: ""
+    }
+  }
+
+  onClickButton = () => {
+    this.setState({ firstName: "Aman", lastName: "Chaudhary" })
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button type="submit" onClick={() =>this.onClickButton()}>Click Me</button>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload {this.props.Name} {this.props.number}.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span>{this.state.firstName}</span>
+            <span>{this.state.lastName}</span>
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
