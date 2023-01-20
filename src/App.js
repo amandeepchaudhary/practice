3f47754458468eaa8890abb7877a48209f0df58e
amandeep <amanchaudhary.ac1@gmail.com>
2023-01-21 00:48:12 +0530

    This is a way to add userid and userName by mapping

--- src/App.js
+++ src/App.js
@@ -11,11 +11,21 @@ class App extends React.Component {
       firstName: "",
       lastName: "",
       showMe: false,
+      lis: [],
     }
   }
-
+  
   onClickButton = () => {
     this.setState({...this.state, showMe:true})
+    // console.log(this.state.lis.length)=0 ans
+  }
+  
+  onClickPush = () => {
+    const lis = this.state.lis;
+    const id = lis.length + 1;
+    const object = {userId: id, userName: 'user'+id};
+    lis.push(object);
+    this.setState({...this.state, lis:lis});
   }
 
   onChange = (e) =>{
@@ -40,7 +50,18 @@ class App extends React.Component {
             <div className="col md-6">
               <input type="text" onChange={(e) => this.onChange(e)} id="firstName" name="firstName" placeholder='Enter your first name' />
               <input type="text" onChange={(e) => this.onChange(e)} id="lastName" name="lastName" placeholder='Enter your last name' />
-              <button type="submit" >Submit</button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col md-6">
+              <button className='btn btn-primary mb-2' type="button" onClick={(e)=>this.onClickPush(e)}>
+                Add User
+              </button>
+            </div>
+            <div className="col md-6">
+              {this.state.lis && this.state.lis.map((item,index)=>{
+                return <div key={`div_${index}`}> <span>Id:{item.userId},</span> <span>Name:{item.userName}</span></div>
+              })}
             </div>
           </div>
           {this.state.showMe && <Header firstName={this.state.firstName} lastName={this.state.lastName}/>}
