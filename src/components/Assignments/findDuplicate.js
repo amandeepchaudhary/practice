0397351f15fe1be12e06bf6f285adb746956cb3d
amandeep <amanchaudhary.ac1@gmail.com>
2023-03-05 16:32:32 +0530

    Assignment for comparing a string having duplicate chars

---
+++ src/components/Assignments/findDuplicate.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+export default function FindDuplicate() {
+    const [com , setCom] = useState("");
+
+    const enter = (e) =>{
+        setCom(e.target.value);
+    }
+    const compare = () =>{
+        // setCom(e.target.value)  //<<<<<Here enter the string you want to compare<<<<<
+        for(var i=0;com.length>i;i++){
+            for(let j=0;com.length>j;j++){
+                // console.log(n[i])
+                if(com[i]===com[j+1+i]){
+                    console.log(com[i])
+                    
+                }
+            }
+        }
+    }
+
+  return (
+    <div>
+        <input type='text' placeholder='Type string to get repeated words!' id='word' name='word' onChange={(e)=>enter(e)}></input>
+        <button onClick={()=>compare()}>Compare</button>
+    </div>
+  )
+}
